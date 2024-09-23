82eae7b45d20da350530a0da51a66ff293fdf575
janee06 <s2022BockovaJana@skolabaltaci.cz>
2024-09-23 08:27:45 +0200

    hello_world

---
+++ eshop_anime/git_config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git config --global user.name "janee06"
+git config --global user.email "s2022BockovaJana@skolabaltaci.cz"
+echo "přihlášení uživatele proběhlo v pořádku"
+#cd eshop_anime
+# php artisan serve
