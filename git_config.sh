71b4106eb2e8e78dfb88ee06ae078ef42962468e
janee06 <s2022BockovaJana@skolabaltaci.cz>
2024-09-24 07:45:49 +0200

    adding laravel breeze

--- git_config.sh
+++ git_config.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+composer create-project laravel/laravel chcidomu
+git config --list
 git config --global user.name "janee06"
 git config --global user.email "s2022BockovaJana@skolabaltaci.cz"
 echo "přihlášení uživatele proběhlo v pořádku"
+ls 
+dir 
 #cd eshop_anime
 # php artisan serve
