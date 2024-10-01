22a1fad759d420000b246ef6c0d66a6c87a2404e
Michal Tesák <tesakmisa@gmail.com>
2024-10-01 08:10:51 +0200

    commit

--- eshop_anime/git_config.sh
+++ eshop_anime/git_config.sh
@@ -4,3 +4,13 @@ git config --global user.email "s2022BockovaJana@skolabaltaci.cz"
 echo "přihlášení uživatele proběhlo v pořádku"
 #cd eshop_anime
 # php artisan serve
+
+# příkazy: 
+# php artisan make:controller NazevKontroleru
+# php artisan make:model NazevModelu
+# php artisan make:migration
+# php artisan make:component NazevKomponenty
+
+# všechny příkazy najednou 
+# php artisan make:model NazevModelu -mcr
+
---
+++ git_config.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#composer create-project laravel/laravel chcidomu
+git config --list
+git config --global --unsset-all user.name
+git config --global --unsset-all user.email
+git config --global user.name "janee06"
+git config --global user.email "s2022BockovaJana@skolabaltaci.cz"
+echo "přihlášení uživatele proběhlo v pořádku"
+ls 
+dir 
+#cd eshop_anime
+# php artisan serve
